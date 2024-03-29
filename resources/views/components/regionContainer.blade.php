e5de152f491ee8fd93307a6e979c5a40770ed7ef
israel silva <israel524.is@gmail.com>
2024-03-28 23:17:03 -0300

    view de ruas e ckecklist de condiçõe

--- resources/views/components/regionContainer.blade.php
+++ resources/views/components/regionContainer.blade.php
@@ -1,6 +1,31 @@
-<div id="regionContainer" class=" bg-white">
+<div id="regionContainer" class="bg-white p-1 d-flex">
+    <button id="ferraButton" class="btn btn-primary">
+        <i class="fa-solid fa-hammer" style="color: #ffffff;"></i>
+    </button>
+    <div id="ferraOptions" class="d-none">
+        <button class="btn btn-primary">
+            <i class="fa-solid fa-magnifying-glass fa-2xs" style="color: #ffffff;"></i>
+            Buscar</button>
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#ruasModal"
+            aria-controls="offcanvasExample">
+            <i class="fa-solid fa-road fa-2xs" style="color: #ffffff;"></i>
+            Ruas
+        </button>
+        <button class="btn btn-primary"><i class="fa-solid fa-brush fa-2xs" style="color: #ffffff;"></i>
+            Temas</button>
+        <button class="btn btn-primary"><i class="fa-solid fa-rotate-right fa-flip-horizontal fa-2xs"
+                style="color: #ffffff;"></i>
+            Desfazer Tudo</button>
+
+
+    </div>
     <select id="regionId" name="regionId">
         <option value="0" selected>Selecione uma região</option>
     </select>
-    <br>
+    <button id="configButton" class="btn btn-primary">
+        <i class="fa-solid fa-gear" style="color: #ffffff;"></i>
+    </button>
+    <button id="voltarButton" class="btn btn-primary">
+        <i class="fa-solid fa-arrow-left-long" style="color: #ffffff;"></i>
+    </button>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="storage/favicon/favicon.svg" type="image/svg+xml">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css" rel="stylesheet">
+
 
 </head>
 
@@ -20,11 +22,15 @@
 
     @include('components.regionContainer')
 
+    {{-- @include('components.configButton') --}}
+
     <main style="padding-top: 45px;"> <!-- 45px é a altura do seu cabeçalho -->
         <div id="map"></div>
     </main>
 
     @include('components.lateral')
+
+    @include('components.ruasModal')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
@@ -32,7 +38,7 @@
 
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script> <!-- Carrega o arquivo Leaflet.js -->
 
-    <script src="{{ asset('storage/js/scripts.js') }}"></script>
+    <script type="module" src="{{ asset('storage/js/scripts.js') }}"></script>
 
     <script src="https://kit.fontawesome.com/bf4bab225b.js" crossorigin="anonymous"></script>
 
