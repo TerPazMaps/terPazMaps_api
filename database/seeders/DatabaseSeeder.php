1a82d4a5776069599776e0611b28c3f91a5fe412
israel silva <Israel524.is@gmail.com>
2024-10-29 18:51:06 -0300

    seeders p1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Subclasse;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ClassesSeeder; // Importa o ClassesSeeder
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Chama o seeder das classes
+        $this->call(ClassesSeeder::class);
+        $this->call(RegionSeeder::class);
+        $this->call(StreetConditionSeeder::class);
+        $this->call(SubclasseSeeder::class);
+        // $this->call(ActivitieSeeder::class);
+        // $this->call(StreetSeeder::class);
+        // $this->call(IconSeeder::class);
+        // $this->call(IconSeeder::class);
+        
+        // Você pode adicionar outros seeders aqui no futuro, se necessário
+        // Exemplo: $this->call(OtherSeeder::class);
+        
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
